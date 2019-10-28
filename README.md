89c2c6bfd10785e403afd146280256c4f2dca573
wesley benicio <wesleybenicio4@gmail.com>
2019-10-28 14:06:56 -0300

    Update: Generate Matriz

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # dam-disaster-simulator
+
+- Simulador de desastres em barragens.
+
+- Tem o objetivo de conseguir mostrar de forma gráfica as áreas afetadas pelo rompimento de barragens.
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from PIL import Image, ImageFilter #pip install pillow
 import numpy as np
 from PegarDados import pickImage
 import os
+import math
 
 #onde pegar os links https://pt-br.topographic-map.com/
 cores = [(255, 255, 255), #1579m
@@ -25,36 +26,57 @@ cores = [(255, 255, 255), #1579m
 (0, 115, 255), #755
 (0, 58, 255),#737
 (0, 0, 255)] #728
+alturas = [1579,1517,1457,1398,1340,1284,1230,1178,1128,1079,1033,989,947,908,871,837,806,778,755,737,728]
 
 url = "https://pt-br.topographic-map.com/maps/g5zv/Brumadinho/"
+def passo1(url= ''):
+    if os.path.isfile("./imagens/"+(url.split('/')[5])+".png"):
+        img = Image.open("./imagens/"+(url.split('/')[5])+".png")
+    else: 
+        img = Image.open("./imagens/"+pickImage.pegarImage(link= url))
 
-if os.path.isfile("./imagens/"+(url.split('/')[5])+".png"):
-    img = Image.open("./imagens/"+(url.split('/')[5])+".png")
-else: 
-    img = Image.open("./imagens/"+pickImage.pegarImage(link= url))
+    #img.show()
+    # img_sharp = img.filter(ImageFilter.SHARPEN) ##filtro na imagem
+    # img_sharp.save('imagens/Brumadinho_sharpened.jpg','JPEG')
+    # r,g,b = img_sharp.split() #separar em rgb
+    size = width,height = img.size;
+    #print(size)
 
-#img.show()
-# img_sharp = img.filter(ImageFilter.SHARPEN) ##filtro na imagem
-# img_sharp.save('imagens/Brumadinho_sharpened.jpg','JPEG')
-# r,g,b = img_sharp.split() #separar em rgb
-size = width,height = img.size;
-#print(size)
+    matriz = []
+    mat1 = []
+    coordenada = 10,10
+    try:
+        for x in range(0,width):
+            for y in range(0,height):
+                coordenada = x,y
+                if img.getpixel(coordenada) not in cores:
+                    (r,g,b) = img.getpixel(coordenada)
+                    dist = []
+                    for cor in cores:
+                        for cor in cores: 
+                            dist.append(distanciaRGB((r,g,b),(cor)))
+                    img.putpixel(coordenada, cores[dist.index(min(dist))])
+                    mat1.append(alturas[dist.index(min(dist))])
+            matriz.append(mat1)
+            mat1 = []
+    except IndexError:
+        print("Erro de out of range")
 
-matriz = []
-coordenada = 10,10
-try:
-    for x in range(0,width):
-        for y in range(0,height):
-            coordenada = x,y
-            matriz.append(img.getpixel(coordenada))
-except IndexError:
-    print("Erro de out of range")
+    print(len(matriz),size)
 
-print(len(matriz),size)
+        
+
+    #img.paste("red",(0,0,1,1))
+    img.show()
+    img.save("./imagens/"+(url.split('/')[5])+"_distance.png")
+
+    # print(list(img.getdata()))
+
+def distanciaRGB(rgb1,rgb2):
+    r1,g1,b1 = rgb1
+    r2,g2,b2 = rgb2
+    d1 = math.sqrt(math.pow(r1-r2,2)+math.pow(g1-g2,2)+  math.pow(b1-b2,2))
+    return d1
 
 
-img.paste("blue",(0,0,20,20))
-img.show()
-
-
-# print(list(img.getdata()))
+passo1(url)
