007621c415b10257febec533ba92c089700099ab
wesley benicio <wesleybenicio4@gmail.com>
2019-10-28 12:12:14 -0300

    pegando imagens

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from PIL import Image, ImageFilter #pip install pillow
 import numpy as np
 from PegarDados import pickImage
+import os
 
 #onde pegar os links https://pt-br.topographic-map.com/
 cores = [(255, 255, 255), #1579m
@@ -25,16 +26,33 @@ cores = [(255, 255, 255), #1579m
 (0, 58, 255),#737
 (0, 0, 255)] #728
 
+url = "https://pt-br.topographic-map.com/maps/g5zv/Brumadinho/"
 
-img = Image.open("imagens/"+pickImage.pegarImage(link= "https://pt-br.topographic-map.com/maps/g5zv/Brumadinho/"))
-img.show()
+if os.path.isfile("./imagens/"+(url.split('/')[5])+".png"):
+    img = Image.open("./imagens/"+(url.split('/')[5])+".png")
+else: 
+    img = Image.open("./imagens/"+pickImage.pegarImage(link= url))
+
+#img.show()
 # img_sharp = img.filter(ImageFilter.SHARPEN) ##filtro na imagem
 # img_sharp.save('imagens/Brumadinho_sharpened.jpg','JPEG')
 # r,g,b = img_sharp.split() #separar em rgb
 size = width,height = img.size;
 #print(size)
+
+matriz = []
 coordenada = 10,10
-print(img.getpixel(coordenada))
+try:
+    for x in range(0,width):
+        for y in range(0,height):
+            coordenada = x,y
+            matriz.append(img.getpixel(coordenada))
+except IndexError:
+    print("Erro de out of range")
+
+print(len(matriz),size)
+
+
 img.paste("blue",(0,0,20,20))
 img.show()
 
